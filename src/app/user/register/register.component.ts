abfcd8f0595ee2943772700e49c15795b6c1a2e3
Robert Gleason <l2ob122@gmail.com>
2023-01-26 19:58:33 -0800

    added form validation for how many chars are needed

---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent {
+  registerForm: FormGroup = new FormGroup({
+    // controls a single form element ( these are all the inputs in the reg form)
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    age: new FormControl('', [Validators.required, Validators.min(18)]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
+  });
+}
